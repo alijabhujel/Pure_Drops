687f757ffc0a7a499e3d1255be773622f448a704
Simran Tamang <simrant009@gmail.com>
2024-08-01 22:20:32 +0545

    workshop form added and connected to db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-
-import Home from './components/ui/Home.js';
+import WorkshopForm from "./components/ui/WorkshopForm.js";
+import Home from "./components/ui/Home.js";
 
 function App() {
   return (
-   <div className="App font-opensans">
-   <Home/>
-   </div>
-  )
+    <div className="App font-opensans">
+      <WorkshopForm />
+    </div>
+  );
 }
 
 export default App;
