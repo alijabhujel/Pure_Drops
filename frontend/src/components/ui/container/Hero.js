061cd8a5b7c3b2b20f250465362030a0b8f9ee2e
alija-bhujel <alijabhujel111@gmail.com>
2024-08-01 18:55:38 +0545

    frontend add

---
+++ frontend/src/components/ui/container/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>
+        Safe Drinking water is most important for us
+      </h1>
+    </div>
+  )
+}
