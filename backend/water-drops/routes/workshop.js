885c069ab750ba10f7c5f073f1bb87156cc70c4d
Simran Tamang <simrant009@gmail.com>
2024-08-01 15:10:33 +0545

    email validation included

--- backend/water-drops/routes/workshop.js
+++ backend/water-drops/routes/workshop.js
@@ -1,24 +1,42 @@
 var express = require("express");
 var router = express.Router();
+const WorkshopForm = require("../models/WorkshopForm");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 /* GET workshop page. */
 router.get("/", function (req, res, next) {
   res.send("Welcome to the workshop page");
 });
 
-router.post("/", function (req, res, next) {
+router.post("/", async function (req, res, next) {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
     return res.status(400).json({ error: "Fill up all the field" });
   }
-  console.log(
-    `Received data: Name=${name}, Email=${email}, "Message" = ${message}`
-  );
-  res.status(200).json({
-    message: "Form data received successfully",
-    data: { name, email, message },
-  });
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+  try {
+    const newForm = new WorkshopForm({
+      name,
+      email,
+      message,
+    });
+    await newForm.save();
+
+    console.log(
+      `Received data: Name=${name}, Email=${email}, "Message" = ${message}`
+    );
+    res.status(200).json({
+      message: "Form data received successfully",
+      data: { name, email, message },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to save data", details: error.message });
+  }
 });
 
 module.exports = router;
