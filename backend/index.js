9cd0d76cf538fe7789ecc3d21a1a383bc0f4361f
Simran Tamang <simrant009@gmail.com>
2024-08-01 11:23:26 +0545

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+//routes
+app.get("/workshop", (req, res) => {
+  console.log("I am here");
+});
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
