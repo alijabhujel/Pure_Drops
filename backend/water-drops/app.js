0e5e3d0cd1c956806bd7842d05dc699551e7a1bd
Pramila Tamang <pramilatmg86@gmail.com>
2024-08-01 15:12:55 +0545

    filtration added

--- backend/water-drops/app.js
+++ backend/water-drops/app.js
@@ -7,9 +7,12 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var workShopRouter = require("./routes/workshop");
+var filtrationRouter = require("./routes/filtration");
+var campaignRoutes = require("./routes/campaign");
 
+
+var mongoose = require("mongoose");
 var app = express();
-
 mongoose.connect("mongodb://localhost/Pure_drop").then(() => {
   console.log("connected to db");
 });
@@ -25,8 +28,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/workshop", workShopRouter);
+app.use("/users",usersRouter);
+app.use("/workshop",workShopRouter);
+app.use("/campaign",campaignRoutes);
+app.use("/filtration",filtrationRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,46 +52,4 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var campaignRoutes = require('./routes/campaign');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/campaign', campaignRoutes)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- backend/water-drops/routes/campaign.js
+++ backend/water-drops/routes/campaign.js
@@ -5,4 +5,25 @@ const router = express.Router();
         res.send("Campaign");
       });
 
+  //route to get all campaigns
+  router.get('/', async(req, res) => {
+    try{
+        const campaigns = await campaign.find();
+        res.status(200).json(campaigns);
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+  });
+
+  //route to get campaign by Id
+  router.get('/:id', async(req, res) => {
+    try{
+      const campaigns = await campaign.find();
+      res.status(200).json(campaigns);
+    } catch (error){
+      res.status(500)
+    }
+  })
+
   module.exports = router;
