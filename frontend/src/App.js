aa0416a66a9ee6f1b6e306a9084fcdb3cc72a41d
alija-bhujel <alijabhujel111@gmail.com>
2024-08-02 09:58:53 +0545

    frontend page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/ui/Home';
+import Games from './components/ui/Games';
+import Quiz from './components/ui/Quiz';
+import Clayvessel from './components/ui/Clayvessel';
+import Clothfilter from './components/ui/Clothfilter';
+import Header from './components/shared/Header';
 
-import Home from './components/ui/Home.js';
+
 
 function App() {
   return (
-   <div className="App font-opensans">
-   <Home/>
-   </div>
-  )
+    <>
+    <Header/>
+    <Routes>
+     
+      <Route index element={<Home/>}/>
+     
+      <Route exact path="/Home" element={<Home/>}/>
+      <Route exact path="/Games" element={<Games/>}/>
+      <Route exact path="/Quiz" element={<Quiz/>}/>
+      <Route exact path="/Clayvessel" element={<Clayvessel/>}/>
+      <Route exact path="/Clothfilter" element={<Clothfilter/>}/>
+       
+       
+       
+     
+    </Routes>
+    </>
+  );
 }
 
 export default App;
